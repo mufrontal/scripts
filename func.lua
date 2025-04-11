aa04b024e7d4250bf72e5116980f3710a28565f0
mufrontal <suporte@mufrontal.com>
2025-04-11 10:55:10 -0300

    Create func.lua

---
+++ func.lua
@@ -0,0 +1,98 @@
+###################################################################################################################################
+NÃO TEM NEM GET E NEM SET                                                                                                         #
+###################################################################################################################################
+getReset() -> get resets       -> (Reset e MReset)
+setReset(value) -> set resets  -> (Reset e MReset)
+
+getDefensePvP() -> get value defense pvp of the player
+setDefensePvP(value) -> set valeu defense pvp of the player
+
+getSuccessBlock() -> get success defense of the player
+setSuccessBlock(value) -> set success defense
+
+getSuccessBlockPvP() -> get success defense pvp of the player
+setSuccessBlockPvP(value) -> set success defense pvp
+
+getTargetShopNumber() -> get the number of the mall that the player is open (npc)
+setTargetShopNumber(value) -> set the number
+
+getWarehouseCount() -> get numbers of warehouse       -> Desnecessário pq vc já tem sistema de compra de baú
+setWarehouseCount(value) -> set numbers of warehouse  -> Desnecessário pq vc já tem sistema de compra de baú
+
+getAttackSpeed() -> get attack speed
+setAttackSpeed(value) -> set attack speed
+
+getMagicSpeed() -> get magic speed
+setMagicSpeed(value) -> set magic speed
+
+getMagicDamageMin() -> get damage min
+setMagicDamageMin(value) -> set damage min
+
+getMagicDamageMax() -> get damage max
+setMagicDamageMax(value) -> set damage max
+
+getAttackDamageMinLeft() -> get attack damage min left
+setAttackDamageMinLeft(value) -> set attack damage min left
+
+getAttackDamageMaxLeft() -> get attack damage max left
+setAttackDamageMaxLeft(value) -> set attack damage max left
+
+getAttackDamageMinRight() -> get attack damage min right
+setAttackDamageMinRight(value) -> set attack damage min right
+
+getAttackDamageMaxRight() -> get attack damage max right
+setAttackDamageMaxRight(value) -> set attack damage max right
+
+getMagicSkill() -> get magic skill player are using -> Usaria para enviar algum pacote pro cliente
+setMagicSkill(value) -> set magic skill
+
+getKillerType() -> get killer type 
+setKillerType(value) -> set killer type
+
+getConnectCheckTime() -> get connected check time
+setConnectCheckTime(value) -> set connected check time
+
+
+###################################################################################################################################
+EU FARIA A PORCENTAGEM EM UM SERVIDOR HARD                                                                                        #
+###################################################################################################################################
+getNextExp() -> get next experience level             	
+setNextExp(value) -> set next experience level        	
+getMasterExperience() -> get experience master level
+setMasterExperience(value) -> set experience master level
+
+###################################################################################################################################
+ESSAS FUNÇÕES VOCÊ JÁ TEM O GET, MAS NÃO O SET                                                                                    #
+###################################################################################################################################
+getLife() -> get life player in real time
+setLife(value) -> set life player
+getMana() -> get mana player in real time
+setMana(value) -> set mana player
+getMaxLife() -> get max life player
+setMaxLife(value) -> set max life player
+getMaxMana() -> get max mana player
+setMaxMana(value) -> set max mana
+getBP() -> get BP player
+setBP(value) -> set BP
+getMaxBP() -> get max BP player
+setMaxBP(value) -> set max BP
+getShield() -> get shield player
+setShield(value) -> set shield
+getMaxShield() -> get max shield player
+setMaxShield(value) -> set max shield
+getDefense() -> get value defense of the player
+setDefense(value) -> set value defense 
+
+
+###################################################################################################################################
+POSSIVELMENTE EXISTEM                                                                                                             #
+###################################################################################################################################
+getTradeTarget() -> get aIndex of the player target are in trade        -> Faria um script para evento automático 
+setTradeTarget(value) -> set aIndex of the player target are in trade   -> Faria um script para evento automático
+
+getExpRate() -> get experience rate                                                                  -> OK
+setExpRate(value) -> set experience rate (you can use for add experience for a STAFF, SWORD etc...)  -> OK
+
+
+
+
